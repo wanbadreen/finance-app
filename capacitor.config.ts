f5fc6f24b65639a35c18cad47df830b2eda57006
wanbadreen <wan@morimotoformulas.com>
2026-09-21 11:30:45 +0800

    Configure Kira for native iOS

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.wanbadreen.kira',
+  appName: 'Kira',
+  webDir: 'dist',
+  ios: {
+    contentInset: 'automatic'
+  }
+};
+
+export default config;
